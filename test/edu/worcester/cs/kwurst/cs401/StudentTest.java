83f70049163a714fe79dd924b28cfcf524d51a66
MLopatka <mlopatka@worcester.edu>
2014-03-31 15:19:12 -0400

    Push changes

---
+++ test/edu/worcester/cs/kwurst/cs401/StudentTest.java
@@ -0,0 +1,35 @@
+package edu.worcester.cs.kwurst.cs401;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentTest {
+
+	
+	private Student student1;
+	@Before
+	public void setUp()
+	{
+	student1 = new Student("Jane", "Smith");
+	}
+
+	@Test
+	public void testGetCurrentEarnedCr() {
+	
+		int credits = 45;
+		student1.setCurrentEarnedCr(credits);
+		assertEquals(credits, student1.getCurrentEarnedCr());
+	}
+	
+
+@Test
+public void testReadyToGraduate1()
+{
+student1.setCurrentEarnedCr(120);
+student1.setGpa(2.0);
+student1.setMajorComplete(true);
+student1.setLascComplete(true);
+assertTrue(student1.readyToGraduate());
+}}
